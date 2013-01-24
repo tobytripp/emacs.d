09e8249a10ea59956188887cd70cd97d50204ea9
Toby Tripp <toby.tripp+github@gmail.com>
2013-01-24 15:57:14 -0600

    Fix fontset issue when run in terminal

--- lib/font-sets.el
+++ lib/font-sets.el
@@ -1,15 +1,16 @@
-
 (set-face-attribute 'default nil
                     :family "Inconsolata" :height 145 :weight 'normal)
 
-;; Fallbacks for missing unicode glyphs
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x21a2)
-                        (decode-char 'ucs #x21a3))
-                  '("Menlo" . "iso10646-1")
-                  )
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x21dd)
-                        (decode-char 'ucs #x2218))
-                  '("Menlo" . "iso10646-1")
-                  )
+(when (functionp 'set-fontset-font)
+  ;; Fallbacks for missing unicode glyphs
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x21a2)
+                          (decode-char 'ucs #x21a3))
+                    '("Menlo" . "iso10646-1")
+                    )
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x21dd)
+                          (decode-char 'ucs #x2218))
+                    '("Menlo" . "iso10646-1")
+                    )
+  )

bda38f9a19d862d3baa9ed94785676b1ecdb2d0e
Toby Tripp <toby.tripp+github@gmail.com>
2013-04-16 16:03:27 -0500

    Update some function key bindings.

--- toby-theme.el
+++ toby-theme.el
@@ -1,5 +1,5 @@
 (deftheme toby
-  "Created 2012-09-13.")
+  "Created 2013-03-27.")
 
 (custom-theme-set-variables
  'toby
@@ -39,11 +39,11 @@
  '(fringe ((((class color) (min-colors 89)) (:foreground "#839496" :background "#073642"))))
  '(header-line ((((class color) (min-colors 89)) (:foreground "#b58900" :background "#073642" :box (:line-width -1 :style released-button)))))
  '(tooltip ((((class color) (min-colors 89)) (:background "#7B6000" :foreground "#DEB542" :inherit variable-pitch))))
- '(mode-line ((((class color) (min-colors 89)) (:foreground "#839496" :background "#073642" :box (:line-width -1 :style released-button)))))
+ '(mode-line ((((class color) (min-colors 89)) (:box (:line-width -1 :style released-button) :background "#073642" :foreground "#839496"))))
  '(mode-line-buffer-id ((((class color) (min-colors 89)) (:foreground "#93a1a1" :weight bold))))
  '(mode-line-emphasis ((t (:weight bold))))
  '(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2 :color "grey40" :style released-button))) (t (:inherit (highlight)))))
- '(mode-line-inactive ((((class color) (min-colors 89)) (:box (:line-width -1 :style released-button) :background "#1c1c1c" :foreground "#839496"))))
+ '(mode-line-inactive ((((class color) (min-colors 89)) (:box (:line-width -1 :style released-button) :background "#000000" :foreground "#839496"))))
  '(isearch ((((class color) (min-colors 89)) (:foreground "#b58900" :background "#073642"))))
  '(isearch-fail ((((class color) (min-colors 89)) (:foreground "#839496" :background "#dc322f"))))
  '(lazy-highlight ((((class color) (min-colors 89)) (:foreground "#b58900" :background "#073642"))))

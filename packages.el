9aa980d4feae17c1ce33424649d1f7c51231da6a
Toby Tripp <toby.tripp+github@gmail.com>
2013-05-27 21:07:26 -0500

    el-get install

--- packages.el
+++ packages.el
@@ -1,3 +1,14 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -71,4 +82,9 @@
     (toby/require-package p)))
 
 (toby/assert-installed-packages)
+
+(el-get 'sync)
+
+
+
 (provide 'packages)

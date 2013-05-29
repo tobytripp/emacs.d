2158f8569e2eee41ae5cc060c8b89cfb62f3f92d
Toby Tripp <toby.tripp+github@gmail.com>
2013-05-28 21:47:10 -0500

    Trialing magit-wip-mode.
    magit-wip-mode is an extension to magit that supports git-wip[1] on
    saving buffers in emacs.
    
     1: https://github.com/bartman/git-wip

--- lib/git-env.el
+++ lib/git-env.el
@@ -1,3 +1,6 @@
 (setenv  "PATH" (concat (getenv "PATH") ":" "/usr/local/bin" ":" "/usr/local/git/bin/"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 (setq exec-path (append exec-path '("/usr/local/git/bin")))
+
+(magit-wip-mode 1)
+(global-magit-wip-save-mode 1)
--- lib/git-wip-init.el
+++
@@ -1,8 +0,0 @@
-(let ((git-wip-dir (concat vendor-dir "git-wip/")))
-  (if (file-exists-p git-wip-dir)
-      (progn
-        (if (not (string-match git-wip-dir (getenv "PATH")))
-            (setenv "PATH" (concat (getenv "PATH") ":" git-wip-dir)))
-
-        (autoload 'git-wip-mode (concat git-wip-dir "emacs/git-wip-mode") "Git WIP mode")
-        )))

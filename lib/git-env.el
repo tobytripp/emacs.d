8cce591ed5bccf65d1467f4627a27b9d5dd8806f
Toby Tripp <toby.tripp+github@gmail.com>
2013-08-01 20:47:31 -0500

    Configure git gutter

--- lib/git-env.el
+++ lib/git-env.el
@@ -4,3 +4,10 @@
 
 (magit-wip-mode 1)
 (global-magit-wip-save-mode 1)
+
+;; https://github.com/nonsequitur/git-gutter-plus
+(require 'git-gutter+)
+(global-git-gutter+-mode t)
+
+(global-set-key (kbd "C-x n") 'git-gutter+-next-hunk)
+(global-set-key (kbd "C-x p") 'git-gutter+-previous-hunk)

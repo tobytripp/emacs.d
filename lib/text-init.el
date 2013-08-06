d415da775985d18530b330c32855ec92ba628443
Toby Tripp <toby.tripp+github@gmail.com>
2013-08-05 19:28:28 -0500

    Start key frequency tracking

--- lib/text-init.el
+++ lib/text-init.el
@@ -1,8 +1,10 @@
-;;(require 'abbrev)
 (setq abbrev-file-name "~/.emacs.d/abbrev_defs")
-;; (add-hook 'text-mode-hook (lambda () (abbrev-mode 1)))
 
 (if (file-exists-p abbrev-file-name)
     (quietly-read-abbrev-file))
 
 (browse-kill-ring-default-keybindings)
+
+(require 'keyfreq)
+(keyfreq-mode 1)
+(keyfreq-autosave-mode 1)

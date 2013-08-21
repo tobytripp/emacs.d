fdc68759a52d3d76b352a773ba7f3edf76e40c39
Toby Tripp <toby.tripp+github@gmail.com>
2013-08-21 17:10:08 -0500

    Add a function to open eshell in current dir.

--- lib/eshell-hooks.el
+++ lib/eshell-hooks.el
@@ -1,3 +1,20 @@
+(defun toby/eshell-in-buffer-dir ()
+  "Open eshell in the current buffer's directory"
+  (interactive)
+  (let ((dir default-directory))
+    (let ((b (get-buffer eshell-buffer-name)))
+      (unless b (eshell))
+      )
+    (display-buffer eshell-buffer-name t)
+    (switch-to-buffer-other-window eshell-buffer-name)
+    (end-of-buffer)
+    (unless (equalp dir default-directory)
+      (cd dir)
+      (eshell-send-input)
+      (end-of-buffer))))
+
+(global-set-key (kbd "C-x C-m") 'toby/eshell-in-buffer-dir)
+
 (add-hook 'eshell-mode-hook
           (lambda ()
             (defalias 'ff 'find-file)

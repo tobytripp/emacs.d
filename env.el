52ca5ad0b1b0914ad927cabcc93499a7d4d870f8
Toby Tripp <toby.tripp+github@gmail.com>
2014-05-24 17:05:03 -0500

    fix up ctags integration

--- env.el
+++ env.el
@@ -1,9 +1,11 @@
-(setenv  "PATH" (concat (getenv "PATH") ":"
-   "/usr/local/bin" ":"
-   "/usr/local/git/bin/" ":"
-   (expand-file-name "~/.cabal/bin")))
+(setenv "PATH"
+	(concat
+	 "/usr/local/bin" ":"
+	 (getenv "PATH") ":"
+	 "/usr/local/git/bin/" ":"
+	 (expand-file-name "~/.cabal/bin")))
 
-(setq exec-path (append exec-path '("/usr/local/bin")))
+(setq exec-path (append '("/usr/local/bin") exec-path))
 (setq exec-path (append exec-path '("/usr/local/git/bin")))
 ;; (setq exec-path (append exec-path (expand-file-name "~/.cabal/bin")))
 

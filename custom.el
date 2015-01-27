26f28dc8a5a642c7f4bd669a354a74152b174720
Toby Tripp <toby.tripp+github@gmail.com>
2015-01-27 09:48:17 -0600

    Update safe themes list.

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   [solarized-bg red green yellow blue magenta cyan solarized-fg])
+ '(ansi-color-names-vector [solarized-bg red green yellow blue magenta cyan solarized-fg])
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 211 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -17,53 +16,18 @@
  '(compilation-scroll-output nil)
  '(css-indent-offset 3)
  '(custom-enabled-themes (quote (tsdh-light)))
- '(custom-safe-themes
-   (quote
-    ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "526e738d6d89317f821becd58d3165b5ecf5afe336ff1c96e331fdfca5e8fe03" default)))
- '(default-frame-alist
-    (quote
-     ((vertical-scroll-bars)
-      (cursor-type . box)
-      (internal-border-width . 0)
-      (left-fringe . 1)
-      (right-fringe)
-      (fringe)
-      (background-color . "#211E1E")
-      (background-mode . dark)
-      (background-toolbar-color . "#cf3ccf3ccf3c")
-      (border-color . "Grey")
-      (bottom-toolbar-shadow-color . "#79e77df779e7")
-      (cursor-color . "#FFFFFF")
-      (foreground-color . "#DADADA")
-      (mouse-color . "Grey")
-      (top-toolbar-shadow-color . "#fffffbeeffff")
-      (viper-saved-cursor-color-in-replace-mode . "Red3"))))
+ '(custom-safe-themes (quote ("025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "bb59851aac88e0319bb855d510c2a63fcbde234b53105149877c2ff34f5c97a6" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "526e738d6d89317f821becd58d3165b5ecf5afe336ff1c96e331fdfca5e8fe03" default)))
+ '(default-frame-alist (quote ((vertical-scroll-bars) (cursor-type . box) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (background-color . "#211E1E") (background-mode . dark) (background-toolbar-color . "#cf3ccf3ccf3c") (border-color . "Grey") (bottom-toolbar-shadow-color . "#79e77df779e7") (cursor-color . "#FFFFFF") (foreground-color . "#DADADA") (mouse-color . "Grey") (top-toolbar-shadow-color . "#fffffbeeffff") (viper-saved-cursor-color-in-replace-mode . "Red3"))))
  '(dired-auto-revert-buffer (quote dired-directory-changed-p))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(electric-pair-mode t)
- '(erc-modules
-   (quote
-    (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom smiley stamp spelling track)))
+ '(erc-modules (quote (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom smiley stamp spelling track)))
  '(erc-server "irc.freenode.net")
  '(ergoemacs-mode nil)
  '(fci-rule-color "#073642")
  '(feature-cucumber-command "bundle exec cucumber -p guard CUCUMBER_OPTS=\"{options}\" {feature}")
  '(flycheck-disabled-checkers (quote (ruby-rubocop)))
- '(flymake-allowed-file-name-masks
-   (quote
-    ((".+\\.lhs$" flymake-Haskell-init flymake-simple-java-cleanup)
-     (".+\\.hs$" flymake-Haskell-init flymake-simple-java-cleanup)
-     ("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init)
-     ("\\.xml\\'" flymake-xml-init)
-     ("\\.html?\\'" flymake-xml-init)
-     ("\\.cs\\'" flymake-simple-make-init)
-     ("\\.p[ml]\\'" flymake-perl-init)
-     ("\\.php[345]?\\'" flymake-php-init)
-     ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup)
-     ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup)
-     ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup)
-     ("\\.tex\\'" flymake-simple-tex-init)
-     ("\\.idl\\'" flymake-simple-make-init))))
+ '(flymake-allowed-file-name-masks (quote ((".+\\.lhs$" flymake-Haskell-init flymake-simple-java-cleanup) (".+\\.hs$" flymake-Haskell-init flymake-simple-java-cleanup) ("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init) ("\\.xml\\'" flymake-xml-init) ("\\.html?\\'" flymake-xml-init) ("\\.cs\\'" flymake-simple-make-init) ("\\.p[ml]\\'" flymake-perl-init) ("\\.php[345]?\\'" flymake-php-init) ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup) ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup) ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) ("\\.tex\\'" flymake-simple-tex-init) ("\\.idl\\'" flymake-simple-make-init))))
  '(glasses-face (quote bold))
  '(glasses-original-separator " ")
  '(glasses-separate-parentheses-p nil)
@@ -85,9 +49,7 @@
  '(menu-bar-mode nil)
  '(ns-tool-bar-display-mode nil t)
  '(ns-tool-bar-size-mode nil t)
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/org/ib-tasks.org" "~/Dropbox/org/my-tasks.org")))
+ '(org-agenda-files (quote ("~/Dropbox/org/ib-tasks.org" "~/Dropbox/org/my-tasks.org")))
  '(revert-without-query (quote (".*")))
  '(rspec-compilation-buffer-name "*specs*")
  '(rspec-rake-command "bundle exec rake")
@@ -99,11 +61,7 @@
  '(ruby-deep-arglist nil)
  '(ruby-deep-indent-paren nil)
  '(ruby-use-encoding-map t)
- '(safe-local-variable-values
-   (quote
-    ((encoding . utf-8)
-     (whitespace-line-column . 80)
-     (lexical-binding . t))))
+ '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t))))
  '(scss-compile-at-save nil)
  '(visual-line-mode nil t)
  '(warning-minimum-level :error)
@@ -116,5 +74,5 @@
  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face :foreground "#586e75"))))
  '(highlight ((t (:background "color-18" :foreground "white"))))
  '(hl-line ((t (:background "#002b36"))))
- '(magit-item-highlight ((t (:inherit highlight))))
+ '(magit-item-highlight ((t (:inherit highlight))) t)
  '(rainbow-delimiters-depth-1-face ((t (:foreground "wheat")))))

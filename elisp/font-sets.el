eef8af27261dc043f6483cb018ba3d2ff45d8968
Toby Tripp <toby.tripp+github@gmail.com>
2015-12-22 12:50:49 -0600

    Emoji!

--- elisp/font-sets.el
+++ elisp/font-sets.el
@@ -2,5 +2,6 @@
 (set-fontset-font "fontset-default"
 		  'unicode
 		  '("Menlo" . "iso10646-1"))
+(set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend)
 
 (provide 'font-sets)

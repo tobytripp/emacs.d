46c85b5f61dcf9afcaec12b119d75347a2d65991
Toby Tripp <toby.tripp+github@gmail.com>
2017-12-10 13:44:53 -0600

    Revert "(Check-point) something wrong with start-up"
    This reverts commit 5feb07c400f5b30a58335bc28ad680ffc48ff201.

--- elisp/init-packages.el
+++ elisp/init-packages.el
@@ -1,18 +1,18 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 (defun require-package (package &optional minimum-version no-refresh)
   "Install the given PACKAGE, optionally requiring a MINIMUM-VERSION.
 
-   If NO-REFRESH is non-nil, do not re-download the package list
-   before installation.  This prevents ininfinite recursion for
-   packages that cannot be found in any of the archives.
+If NO-REFRESH is non-nil, do not re-download the package list
+before installation.  This prevents ininfinite recursion for
+packages that cannot be found in any of the archives.
 
-   Source:
-     https://github.com/purcell/emacs.d/blob/master/lisp/init-elpa.el"
+Source:
+  https://github.com/purcell/emacs.d/blob/master/lisp/init-elpa.el"
   (if (package-installed-p package minimum-version)
       t
     (if (or (assoc package package-archive-contents) no-refresh)

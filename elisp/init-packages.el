2ee0ad8c2a98f97c0edc44600c3ffabb9f999e4b
Toby Tripp <toby.tripp+github@gmail.com>
2018-09-05 10:46:10 -0500

    Clean up

--- elisp/init-packages.el
+++ elisp/init-packages.el
@@ -4,8 +4,8 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  ;;(add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t))
 (package-initialize)
 
